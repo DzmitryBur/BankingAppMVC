31422b6ff8f087f21fda95562a339fc939036a79
Dzmitry Buracheuski <dmitry.burr@gmail.com>
2022-07-08 15:34:14 +0300

    some changes

--- src/main/java/com/cot/bankingappmvc/Main.java
+++ src/main/java/com/cot/bankingappmvc/Main.java
@@ -3,8 +3,10 @@ package com.cot.bankingappmvc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan("com.cot.bankingappmvc")
+@EnableWebMvc // поставил заранее... см лекцию п.8
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
